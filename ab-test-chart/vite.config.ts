8837666fd67b9e777a64065d1e3498464d80861a
artashesfrangyan <frangyan99@gmail.com>
2025-11-12 00:34:31 +0300

    environment setup

--- ab-test-chart/vite.config.ts
+++ ab-test-chart/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/ab-test-chart/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
+  },
+  server: {
+    port: 3000,
+  },
+});
