4d9c26f8fc6292e65776604f679e8d9a90a058b9
artashesfrangyan <frangyan99@gmail.com>
2025-11-12 05:23:00 +0300

    changed root directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ab-test-chart/",
+  base: "/Kameleoon/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
